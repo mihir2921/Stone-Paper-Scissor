f07ccf6e0f3d6efd0b9507e112ddf45e5d17829f
Mihir Kumar Jha <mihirj@cloudera.com>
2023-12-10 16:37:06 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rock Paper Sicssor
+
+Hi this th Rock Paper Sicssor Game Created By Mihir Kumar Jha
+
+Below are few screenshot of the project
+
+I haves used HTML CSS, and JS to create this game
---
+++ script.js
@@ -0,0 +1,56 @@
+let humanScore = document.querySelector(".human_score")
+let compScore = document.querySelector(".comp_score")
+const result = document.querySelector(".result")
+const choices= document.querySelectorAll(".choice")
+const reset = document.querySelector(".reset")
+
+const winnerCombination={
+    1:3,
+    2:1,
+    3:2
+}
+let huScore = 0
+let cpScore = 0
+let computerChoices= ()=> 
+{
+    return Math.floor(Math.random()*3+1)
+}
+
+choices.forEach((choice)=>{
+        choice.addEventListener("click", ()=>{
+
+            let userChoice = choice.getAttribute("id")
+            console.log(userChoice)
+            let computerChoice = computerChoices()
+            console.log(computerChoice)
+            if(userChoice==computerChoice){
+                result.innerText="Its a DRAW"
+                result.classList.remove("loss")
+                result.classList.remove("won")
+            }
+            else{
+                if(userChoice == winnerCombination[computerChoice]){
+                    result.innerText="Player Won"
+                    result.classList.remove("loss")
+                    result.classList.add("won")
+                    huScore+=1;
+                    humanScore.innerText=huScore
+
+                }
+                else{
+                    result.innerText="Computer Won"
+                    result.classList.add("loss")
+                    result.classList.remove("won")
+                    cpScore+=1
+                    compScore.innerText=cpScore
+                }
+
+        }
+})})
+
+reset.addEventListener("click", ()=>{
+    humanScore.innerText=0;
+    compScore.innerText=0;
+    result.classList.remove("loss")
+    result.classList.remove("won")
+})
